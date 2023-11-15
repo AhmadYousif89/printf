2a18a2bb9cf8a1d49bda3aca7d3cc947fba27b13
AhmadYousif89 <ayob.4wrk89@gmail.com>
2023-11-15 02:20:46 +0200

    t6

--- _printf.c
+++ _printf.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * _printf - prints anything
- * @format: the format string
- *
- * Return: number of bytes printed
+ * _printf - Custom Printf
+ * @format: pointer to the given string
+ * Return: the number of characters given to the function
  */
 int _printf(const char *format, ...)
 {
@@ -15,31 +14,32 @@ int _printf(const char *format, ...)
 	va_list(vlp);
 	va_start(vlp, format);
 
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
+	if (!format
+		|| (format[0] == '%' && !format[1])
+		|| (format[0] == '%' && format[1] == ' ' && !format[2]))
 		return (-1);
 
 	for (s = (char *)format; *s; s++)
 	{
 		init_flags(&flags, vlp);
 		if (*s != '%')
-		{
+		{ /* Skip chars and claculate the length til we get to (%) */
 			len += _putchar(*s);
 			continue;
 		}
-		start = s;
+		start = s; /* Mark starting position */
 		s++;
-		if (get_flag(s, &flags))
-			s++;
-		if (get_modifier(s, &flags))
-			s++;
+
+		while (get_flag(s, &flags))
+			s++; /* If we have a flag move tp next char */
 		s = get_width(s, vlp, &flags);
 		s = get_percision(s, vlp, &flags);
+		if (get_modifier(s, &flags))
+			s++; /* If we have a modifier move tp next char */
 		if (get_matcher(s))
 			len += _print_match_fnc(s, vlp, &flags);
 		else
-			len += _print_range(start, s, flags._long || flags._short ? s - 1 : 0);
+			len += _print_range(start, s, flags._short || flags._long ? s - 1 : 0);
 	}
 
 	_putchar(BUFFER_FLUSH);
